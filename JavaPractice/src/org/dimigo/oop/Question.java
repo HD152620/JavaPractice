486aed65fd45218f4f41aedef24ca671f923b599
chja2 <chja2@DESKTOP-4RHBIO0>
2017-04-03 12:50:07 +0900

    실습과제 7번 입니다.

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    |_ Question
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 3. 31.
+ * </pre>
+ *
+ * @author      :  chja2
+ * @version      : 1.0
+ */
+public class Question {
+
+	/**
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		String[] questions = {"1. 가장 좋아하는 가수는?", "2. 가장 좋아하는 배우는?", "3. 가장 좋아하는 과목은?"};
+		String[] answers = {"빅뱅", "공유", "응용프로그래밍"};
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = 0; i < 3; i++){
+			System.out.println(questions[i]);
+			String answer = scanner.nextLine();
+			if(answers[i].equals(answer)){
+				System.out.println("정답입니다");
+			}
+			else
+				System.out.println("틀렸습니다!");
+	}
+		System.out.println("<< 결과 출력 >>");
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 3; i++){
+			sb.append(questions[i]).append(answers[i]).append("입니다\n");
+		}
+		System.out.println(sb);
+	}
+	
+	
+	
+}
